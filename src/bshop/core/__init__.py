49d55ebb2392cdb7ef86b4f520ef8144cef850ae
mono <mohamadnobakht@gmail.com>
2019-04-29 21:43:40 +0430

    1. docstring revised.

--- src/bshop/core/__init__.py
+++ src/bshop/core/__init__.py
@@ -9,9 +9,10 @@ __app__ = None
 def _set_app(app):
     """
     sets the global instance of running application.
-    this instance should not be accessed directly from packages outside of `bshop.core` package.
-    outside packages, instead should access application functionality
-    through `bshop.core.application.services` module.
+    this instance should not be accessed directly from packages outside of `bshop.core.application`
+    package. but some internal `bshop.core` packages could use this instance if necessary for overcoming
+    circular dependency problem. outside packages, instead should always access application
+    functionality through `bshop.core.application.services` module.
 
     :param Application app: application instance.
     """
