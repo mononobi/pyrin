4460072dbdeb00c60b6a65aedb0a0454c0ea97d4
MoNo <mohamadnobakht@gmail.com>
2019-04-29 14:07:26 +0430

    docstring made lowercase.

--- src/start.py
+++ src/start.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Main entry point for bshop server.
-It should be run without debug flag in production environments.
+main entry point for bshop server.
+it should be run without debug flag in production environments.
 """
 
 from bshop.core.application.base import Application
