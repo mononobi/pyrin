8c3f041815af901348675ec824ab50617c2ae12e
mono <mohamadnobakht@gmail.com>
2019-04-29 21:47:21 +0430

    1. docstring revised.

--- src/bshop/core/__init__.py
+++ src/bshop/core/__init__.py
@@ -24,6 +24,10 @@ def _set_app(app):
 def _get_app():
     """
     gets the current instance of running application.
+    this instance should not be accessed directly from packages outside of `bshop.core.application`
+    package. but some internal `bshop.core` packages could use this instance if necessary for overcoming
+    circular dependency problem. outside packages, instead should always access application
+    functionality through `bshop.core.application.services` module.
 
     :rtype: Application
     """
