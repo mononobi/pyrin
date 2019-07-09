37f9a5a5464999289524d3c31818a6e2f5ba8eb4
mono <mohamadnobakht@gmail.com>
2019-07-09 17:57:55 +0430

    1. start_test script modified.

--- src/start_test.py
+++ src/start_test.py
@@ -3,10 +3,13 @@
 main entry point for pyrin test server.
 """
 
+import pytest
+
 from tests import PyrinTestApplication
+
 
 # the if condition is to ensure that multiprocessing
 # on windows works as expected.
 if __name__ == '__main__':
     app = PyrinTestApplication()
-    app.run(use_reloader=False)
+    pytest.main()
