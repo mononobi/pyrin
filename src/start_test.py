89c1b0e34ee1472e2b9dbdd0c5905bc10889f43b
mono <mohamadnobakht@gmail.com>
2019-07-09 18:33:02 +0430

    1. start_test script modified to cleanup caches after all tests have been executed.

--- src/start_test.py
+++ src/start_test.py
@@ -3,9 +3,24 @@
 main entry point for pyrin test server.
 """
 
+import os
+
 import pytest
 
+import pyrin.utils.path as path_utils
+
 from tests import PyrinTestApplication
+
+
+def cleanup():
+    """
+    cleanups pytest caches after running all tests.
+    """
+
+    root_path = path_utils.resolve_application_root_path()
+    cache_path = os.path.join(root_path, '.pytest_cache')
+    command = 'rm -r {path}'.format(path=cache_path)
+    os.system(command)
 
 
 # the if condition is to ensure that multiprocessing
@@ -13,3 +28,4 @@ from tests import PyrinTestApplication
 if __name__ == '__main__':
     app = PyrinTestApplication()
     pytest.main()
+    cleanup()
