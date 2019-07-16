bd1e63dd66e0c719fc3ba0435a1ddd81e9b6c8bd
mono <mohamadnobakht@gmail.com>
2019-07-16 19:44:16 +0430

    1. request and response object datetime fixed to be get from utc_now().

--- src/pyrin/application/context.py
+++ src/pyrin/application/context.py
@@ -3,11 +3,10 @@
 application context module.
 """
 
-from datetime import datetime
-
 from flask import Request, Response, jsonify
 
 import pyrin.utils.unique_id as uuid_utils
+import pyrin.utils.datetime as datetime_utils
 
 from pyrin.application.exceptions import ComponentAttributeError, InvalidComponentNameError
 from pyrin.core.context import Context, CoreObject, DTO
@@ -126,7 +125,7 @@ class CoreResponse(Response):
 
         self.request_id = None
         self.request_date = None
-        self.response_date = datetime.utcnow()
+        self.response_date = datetime_utils.utc_now()
         self.user = None
         self.context = Context()
 
@@ -162,7 +161,7 @@ class CoreRequest(Request):
         super(CoreRequest, self).__init__(environ, populate_request, shallow)
 
         self.request_id = uuid_utils.generate_uuid4()
-        self.request_date = datetime.utcnow()
+        self.request_date = datetime_utils.utc_now()
         self.user = None
         self.component_custom_key = DEFAULT_COMPONENT_KEY
         self.client_ip = self._get_client_ip()
