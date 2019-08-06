2845291efef513da7cbf8d09fe36b39bbe047721
mono <mohamadnobakht@gmail.com>
2019-08-06 20:31:15 +0430

    1. install script enhanced. 2. install-dependencies script added.

---
+++ scripts/setup/install-dependencies.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+force_update="n"
+# shellcheck disable=SC2039
+read -r -p "Force update all installed dependencies? [y/N] " force_update
+
+force_update_length=${#force_update}
+if [ "$force_update_length" = "0" ]
+then
+    force_update="n"
+fi
+
+# installing gcc.
+gcc_path=$(command -v gcc)
+gcc_length=${#gcc_path}
+
+if [ "$gcc_length" = "0" ] || [ $force_update = "Y" ] || [ $force_update = "y" ]
+then
+    echo "Installing gcc."
+    apt-get install gcc
+else
+    echo "gcc is already installed."
+fi
+
+# installing syslog.
+syslog_path=$(command -v syslog-ng)
+syslog_length=${#syslog_path}
+
+if [ "$syslog_length" = "0" ] || [ $force_update = "Y" ] || [ $force_update = "y" ]
+then
+    echo "Installing syslog."
+    apt-get install syslog-ng-core
+else
+    echo "syslog is already installed."
+fi
+
+# installing python3.7
+python_path=$(command -v python3.7)
+python_length=${#python_path}
+
+if [ "$python_length" = "0" ] || [ $force_update = "Y" ] || [ $force_update = "y" ]
+then
+    echo "Installing python3.7 and dependencies."
+    apt update
+    apt install software-properties-common
+    add-apt-repository ppa:deadsnakes/ppa
+    apt install python3.7
+    apt-get install python3-pip
+else
+    echo "python3.7 is already installed."
+fi
+
+# installing pipenv.
+pipenv_path=$(command -v pipenv)
+pipenv_length=${#pipenv_path}
+
+if [ "$pipenv_length" = "0" ] || [ $force_update = "Y" ] || [ $force_update = "y" ]
+then
+    echo "Installing pipenv."
+    pip install pipenv
+else
+    echo "pipenv is already installed."
+fi
---
+++ scripts/setup/install.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+
+# installing dependencies.
+./install-dependencies.sh
+
+# getting working directory path.
+working_dir=$(pwd)
+
+# user name that application should be accessible to it.
+user_name=app_user
+
+# checking that user_name is present, if not, create it.
+user_exists=$(id -u $user_name > /dev/null 2>&1; echo $?)
+
+if [ "$user_exists" != "0" ]
+then
+    # user does not exist, creating it.
+    echo "Creating $user_name."
+    adduser $user_name
+else
+    echo "$user_name is already present."
+fi
+
+# adding user_name into www-data and sudo groups.
+usermod -aG sudo $user_name
+usermod -aG www-data $user_name
+
+# making logging directory and log backup.
+if [ ! -d "/var/log/backup/pyrin" ]
+then
+    mkdir -p /var/log/backup/pyrin/
+fi
+
+if [ -d "/var/log/pyrin" ]
+then
+    echo "Archiving and cleaning up previous logs."
+    name=$(date "+%Y.%m.%d.%H.%M.%S")
+    tar -czf /var/log/backup/pyrin/"pyrin.log.$name.tar.gz" /var/log/pyrin
+    rm -r /var/log/pyrin
+fi
+
+mkdir -p /var/log/pyrin/
+
+# setting permissions for log directory.
+chown -R root:www-data /var/log/pyrin/
+chmod -R 770 /var/log/pyrin/
+
+# making application backup directory and file.
+if [ ! -d "/var/app_root/backup" ]
+then
+    mkdir -p /var/app_root/backup/
+fi
+
+if [ -d "/var/app_root/pyrin_framework" ]
+then
+    echo "Archiving and cleaning up previous installation."
+    name=$(date "+%Y.%m.%d.%H.%M.%S")
+    tar -czf /var/app_root/backup/"pyrin_framework.$name.tar.gz" /var/app_root/pyrin_framework
+    rm -r /var/app_root/pyrin_framework
+else
+    echo "Performing fresh installation."
+fi
+
+# making up required directory.
+mkdir -p /var/app_root/pyrin_framework/app/
+chown -R $user_name /var/app_root/
+
+echo "Copying applications."
+# copying tests application.
+cp -r ../../src/tests/ /var/app_root/pyrin_framework/app/tests/
+cp ../../src/start_test.py /var/app_root/pyrin_framework/app/start_test.py
+
+# copying pyrin application.
+cp -r ../../src/pyrin/ /var/app_root/pyrin_framework/app/pyrin/
+
+# copying .env file.
+cp ../../src/.env /var/app_root/pyrin_framework/app/.env
+
+# copying pipenv required files.
+cp ../../Pipfile.lock /var/app_root/pyrin_framework/Pipfile.lock
+cp ../../Pipfile /var/app_root/pyrin_framework/Pipfile
+
+# setting up pipenv environment.
+cd /var/app_root/pyrin_framework/ || exit 1
+export PIPENV_VENV_IN_PROJECT=1
+pipenv install --ignore-pipfile
+
+#returning to working directory.
+cd "$working_dir" || exit 1
+
+# setting the owner of directory.
+chown -R $user_name /var/app_root/
+
+echo
+echo "\e[0;32m ** Installation completed ** \e[0m"
+echo
