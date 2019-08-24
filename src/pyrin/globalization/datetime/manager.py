05d1afd299b8b16d6e1e0fe2fdee89bed30a6e8b
mono <mohamadnobakht@gmail.com>
2019-08-24 18:03:55 +0430

    1. DateTimeManager initial implementation.

--- src/pyrin/globalization/datetime/manager.py
+++ src/pyrin/globalization/datetime/manager.py
@@ -3,6 +3,12 @@
 datetime manager module.
 """
 
+from datetime import datetime
+
+import pytz
+
+import pyrin.configuration.services as config_services
+
 from pyrin.core.context import CoreObject
 
 
@@ -17,3 +23,33 @@ class DateTimeManager(CoreObject):
         """
 
         CoreObject.__init__(self)
+
+        default_timezone = config_services.get('globalization', 'datetime', 'default_timezone')
+        self.__active_timezone = pytz.timezone(default_timezone)
+
+    def get_current_datetime(self):
+        """
+        gets current datetime in application default timezone.
+
+        :rtype: datetime
+        """
+
+        return datetime.now(self.get_active_timezone())
+
+    def get_normalized_datetime(self, value):
+        """
+        normalizes the input value to application default timezone.
+
+        :param datetime value: value to get it's application default timezone equivalent.
+
+        :rtype: datetime
+        """
+
+    def get_active_timezone(self):
+        """
+        gets the application active timezone.
+
+        :rtype: tzinfo
+        """
+
+        return self.__active_timezone
