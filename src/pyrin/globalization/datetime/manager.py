c1573ae0e8c950f9c83157986c03e8ccbb30f9be
mono <mohamadnobakht@gmail.com>
2019-10-02 20:10:03 +0330

    1. globalization.datetime package implemented.

--- src/pyrin/globalization/datetime/manager.py
+++ src/pyrin/globalization/datetime/manager.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import pytz
 
 import pyrin.configuration.services as config_services
+import pyrin.utils.datetime as datetime_utils
 
 from pyrin.core.context import CoreObject
 
@@ -24,8 +25,10 @@ class DateTimeManager(CoreObject):
 
         CoreObject.__init__(self)
 
-        default_timezone = config_services.get('globalization', 'datetime', 'default_timezone')
-        self.__current_timezone = pytz.timezone(default_timezone)
+        default_timezone_name = config_services.get('globalization',
+                                                    'datetime',
+                                                    'default_timezone')
+        self.__current_timezone = pytz.timezone(default_timezone_name)
 
     def now(self):
         """
@@ -44,3 +47,130 @@ class DateTimeManager(CoreObject):
         """
 
         return self.__current_timezone
+
+    def get_current_timezone_name(self):
+        """
+        gets the application current timezone name.
+
+        :rtype: str
+        """
+
+        return self.get_current_timezone().zone
+
+    def normalize(self, value):
+        """
+        normalizes input datetime value using application current
+        timezone and returns it.
+        input value should already have a timezone info.
+
+        :param datetime value: value to get normalized.
+
+        :rtype: datetime
+        """
+
+        return self.get_current_timezone().normalize(value)
+
+    def localize(self, value):
+        """
+        localizes input datetime with application current timezone.
+        input value should not have a timezone info.
+
+        :param datetime value: value to be localized.
+
+        :rtype: datetime
+        """
+
+        return self.get_current_timezone().localize(value)
+
+    def _add_timezone_info(self, value):
+        """
+        adds the application current timezone info into input
+        value if it has no timezone info.
+
+        :param Union[datetime, date] value: value to add timezone info into it.
+
+        :rtype: Union[datetime, date]
+        """
+
+        localized_value = value
+        if value.tzinfo is None:
+            localized_value = self.get_current_timezone().localize(value)
+
+        return localized_value
+
+    def to_datetime_string(self, value):
+        """
+        gets the datetime string representation of input value.
+        if the value has no timezone info, it adds the application
+        current timezone info into it.
+        example: `2015-12-24T22:40:15+01:00`
+
+        :param datetime value: input object to be converted.
+
+        :rtype: str
+        """
+
+        localized_value = self._add_timezone_info(value)
+        return datetime_utils.to_datetime_string(localized_value)
+
+    def to_date_string(self, value):
+        """
+        gets the date string representation of input with default format.
+        if the value has no timezone info, it adds the application
+        current timezone info into it.
+        example: `2015-12-24`
+
+        :param Union[datetime, date] value: input object to be converted.
+
+        :rtype: str
+        """
+
+        return datetime_utils.to_date_string(value)
+
+    def to_time_string(self, value):
+        """
+        gets the time string representation of input datetime with utc offset.
+        example: `23:40:15+03:30`
+
+        :param Union[datetime, time] value: input object to be converted.
+
+        :rtype: str
+        """
+
+        localized_value = self._add_timezone_info(value)
+        return datetime_utils.to_time_string(localized_value)
+
+    def to_datetime(self, value):
+        """
+        converts the input value to it's equivalent python datetime.
+
+        :param str value: string representation of datetime to be converted.
+
+        :rtype: datetime
+        """
+
+        converted_datetime = datetime_utils.to_datetime(value)
+        return self._add_timezone_info(converted_datetime)
+
+    def to_date(self, value):
+        """
+        converts the input value to it's equivalent python date.
+
+        :param str value: string representation of date to be converted.
+
+        :rtype: date
+        """
+
+        return datetime_utils.to_date(value)
+
+    def to_time(self, value):
+        """
+        converts the input value to it's equivalent python time.
+
+        :param str value: string representation of time to be converted.
+
+        :rtype: time
+        """
+
+        converted_time = datetime_utils.to_time(value)
+        return self._add_timezone_info(converted_time)
--- src/pyrin/globalization/datetime/services.py
+++ src/pyrin/globalization/datetime/services.py
@@ -25,3 +25,118 @@ def get_current_timezone():
     """
 
     return get_component(DateTimePackage.COMPONENT_NAME).get_current_timezone()
+
+
+def get_current_timezone_name():
+    """
+    gets the application current timezone name.
+
+    :rtype: str
+    """
+
+    return get_component(DateTimePackage.COMPONENT_NAME).get_current_timezone_name()
+
+
+def normalize(value):
+    """
+    normalizes input datetime value using application current timezone.
+    input value should already have a timezone info.
+
+    :param datetime value: value to get normalized.
+
+    :rtype: datetime
+    """
+
+    return get_component(DateTimePackage.COMPONENT_NAME).normalize(value)
+
+
+def localize(value):
+    """
+    localizes input datetime with application current timezone.
+    input value should not have a timezone info.
+
+    :param datetime value: value to be localized.
+
+    :rtype: datetime
+    """
+
+    return get_component(DateTimePackage.COMPONENT_NAME).localize(value)
+
+
+def to_datetime_string(value):
+    """
+    gets the datetime string representation of input value.
+    if the value has no timezone info, it adds the application
+    current timezone info into it.
+    example: `2015-12-24T22:40:15+01:00`
+
+    :param datetime value: input object to be converted.
+
+    :rtype: str
+    """
+
+    return get_component(DateTimePackage.COMPONENT_NAME).to_datetime_string(value)
+
+
+def to_date_string(value):
+    """
+    gets the date string representation of input with default format.
+    if the value has no timezone info, it adds the application
+    current timezone info into it.
+    example: `2015-12-24`
+
+    :param Union[datetime, date] value: input object to be converted.
+
+    :rtype: str
+    """
+
+    return get_component(DateTimePackage.COMPONENT_NAME).to_date_string(value)
+
+
+def to_time_string(value):
+    """
+    gets the time string representation of input datetime with utc offset.
+    example: `23:40:15+03:30`
+
+    :param Union[datetime, time] value: input object to be converted.
+
+    :rtype: str
+    """
+
+    return get_component(DateTimePackage.COMPONENT_NAME).to_time_string(value)
+
+
+def to_datetime(value):
+    """
+    converts the input value to it's equivalent python datetime.
+
+    :param str value: string representation of datetime to be converted.
+
+    :rtype: datetime
+    """
+
+    return get_component(DateTimePackage.COMPONENT_NAME).to_datetime(value)
+
+
+def to_date(value):
+    """
+    converts the input value to it's equivalent python date.
+
+    :param str value: string representation of date to be converted.
+
+    :rtype: date
+    """
+
+    return get_component(DateTimePackage.COMPONENT_NAME).to_date(value)
+
+
+def to_time(value):
+    """
+    converts the input value to it's equivalent python time.
+
+    :param str value: string representation of time to be converted.
+
+    :rtype: time
+    """
+
+    return get_component(DateTimePackage.COMPONENT_NAME).to_time(value)
