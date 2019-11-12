7bc9f4bcd30c4e3fb1766f0d75a007ae07044443
mono <mohamadnobakht@gmail.com>
2019-11-12 20:01:30 +0330

    1. utils.database modified to consider backends which does not support cross database references.

--- src/pyrin/utils/database.py
+++ src/pyrin/utils/database.py
@@ -6,17 +6,24 @@ utils database module.
 from pyrin.core.exceptions import CoreValueError
 
 
-def get_schema_name(database_name, schema_name):
+def get_schema_name(schema_name, database_name=None):
     """
     gets fully qualified schema name for given database and schema.
+    if database name is not provided, it returns the schema name only.
+
+    note that for database backends which does not support cross
+    database references, you should leave `database_name=None`.
+
+    :param str schema_name: schema name.
 
     :param str database_name: database name.
-    :param str schema_name: schema name.
+                              if not provided, database name
+                              will be excluded from result string.
 
     :raises CoreValueError: core value error.
 
     :returns: fully qualified schema name in the form of:
-              `database_name.schema_name`
+              `database_name.schema_name` or `schema_name`
 
     :rtype: str
     """
@@ -27,37 +34,44 @@ def get_schema_name(database_name, schema_name):
     if schema_name is None:
         schema_name = ''
 
-    if database_name.strip() == '':
-        raise CoreValueError('Database name could not be blank.')
+    database_name = database_name.strip()
+    schema_name = schema_name.strip()
 
-    if schema_name.strip() == '':
+    if schema_name == '':
         raise CoreValueError('Schema name could not be blank.')
 
-    qualified_schema_name = '{database}.{schema}'.format(database=database_name,
-                                                         schema=schema_name)
+    if database_name == '':
+        return schema_name
 
-    return qualified_schema_name
+    return '{database}.{schema}'.format(database=database_name, schema=schema_name)
 
 
-def get_column_name(database_name, schema_name, table_name, column_name):
+def get_column_name(schema_name, table_name, column_name, database_name=None):
     """
     gets fully qualified column name for given
     database and schema and table and column.
 
-    :param str database_name: database name.
+    note that for database backends which does not support cross
+    database references, you should leave `database_name=None`.
+
     :param str schema_name: schema name.
     :param str table_name: table name.
     :param str column_name: column name.
 
+    :param str database_name: database name.
+                              if not provided, database name
+                              will be excluded from result string.
+
     :raises CoreValueError: core value error.
 
     :returns: fully qualified column name in the form of:
-              `database_name.schema_name.table_name.column_name`
+              `database_name.schema_name.table_name.column_name` or
+              `schema_name.table_name.column_name`
 
     :rtype: str
     """
 
-    qualified_schema = get_schema_name(database_name, schema_name)
+    qualified_schema = get_schema_name(schema_name, database_name)
 
     if table_name is None:
         table_name = ''
@@ -65,14 +79,15 @@ def get_column_name(database_name, schema_name, table_name, column_name):
     if column_name is None:
         column_name = ''
 
-    if table_name.strip() == '':
+    table_name = table_name.strip()
+    column_name = column_name.strip()
+
+    if table_name == '':
         raise CoreValueError('Table name could not be blank.')
 
-    if column_name.strip() == '':
+    if column_name == '':
         raise CoreValueError('Column name could not be blank.')
 
-    qualified_column_name = '{schema}.{table}.{column}'.format(schema=qualified_schema,
-                                                               table=table_name,
-                                                               column=column_name)
-
-    return qualified_column_name
+    return '{schema}.{table}.{column}'.format(schema=qualified_schema,
+                                              table=table_name,
+                                              column=column_name)
