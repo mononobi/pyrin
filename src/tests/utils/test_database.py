922804b9210f610e42357503954c6ec0866d689b
mono <mohamadnobakht@gmail.com>
2019-11-12 20:02:32 +0330

    1. tests.utils.test_database modified.

--- src/tests/utils/test_database.py
+++ src/tests/utils/test_database.py
@@ -15,19 +15,25 @@ def test_get_schema_name():
     gets fully qualified schema name for given database and schema.
     """
 
-    schema_name = database_utils.get_schema_name('database', 'schema')
+    schema_name = database_utils.get_schema_name('schema', 'database')
 
     assert schema_name == 'database.schema'
 
 
-def test_get_schema_name_with_invalid_database():
+def test_get_schema_name_without_database():
     """
-    gets fully qualified schema name for given invalid database and schema.
-    it should raise an error.
+    gets fully qualified schema name without specifying database name.
     """
 
-    with pytest.raises(CoreValueError):
-        database_utils.get_schema_name(None, 'schema')
+    schema_name1 = database_utils.get_schema_name('schema')
+    schema_name2 = database_utils.get_schema_name('schema', None)
+    schema_name3 = database_utils.get_schema_name('schema', '')
+    schema_name4 = database_utils.get_schema_name('schema', '  ')
+
+    assert schema_name1 == 'schema'
+    assert schema_name2 == 'schema'
+    assert schema_name3 == 'schema'
+    assert schema_name4 == 'schema'
 
 
 def test_get_schema_name_with_invalid_schema():
@@ -37,7 +43,7 @@ def test_get_schema_name_with_invalid_schema():
     """
 
     with pytest.raises(CoreValueError):
-        database_utils.get_schema_name('database', '  ')
+        database_utils.get_schema_name('  ', 'database')
 
 
 def test_get_column_name():
@@ -46,20 +52,26 @@ def test_get_column_name():
     database and schema and table and column.
     """
 
-    column_name = database_utils.get_column_name('database', 'schema', 'table', 'column')
+    column_name = database_utils.get_column_name('schema', 'table', 'column', 'database')
 
     assert column_name == 'database.schema.table.column'
 
 
-def test_get_column_name_with_invalid_database():
+def test_get_column_name_without_database():
     """
-    gets fully qualified column name for given invalid
-    database and schema and table and column.
-    it should raise an error.
+    gets fully qualified column name for given schema
+    and table and column without specifying database name.
     """
 
-    with pytest.raises(CoreValueError):
-        database_utils.get_column_name(None, 'schema', 'table', 'column')
+    column_name1 = database_utils.get_column_name('schema', 'table', 'column')
+    column_name2 = database_utils.get_column_name('schema', 'table', 'column', None)
+    column_name3 = database_utils.get_column_name('schema', 'table', 'column', '')
+    column_name4 = database_utils.get_column_name('schema', 'table', 'column', '  ')
+
+    assert column_name1 == 'schema.table.column'
+    assert column_name2 == 'schema.table.column'
+    assert column_name3 == 'schema.table.column'
+    assert column_name4 == 'schema.table.column'
 
 
 def test_get_column_name_with_invalid_schema():
@@ -70,7 +82,7 @@ def test_get_column_name_with_invalid_schema():
     """
 
     with pytest.raises(CoreValueError):
-        database_utils.get_column_name('database', '  ', 'table', 'column')
+        database_utils.get_column_name('  ', 'table', 'column', 'database')
 
 
 def test_get_column_name_with_invalid_table():
@@ -81,7 +93,7 @@ def test_get_column_name_with_invalid_table():
     """
 
     with pytest.raises(CoreValueError):
-        database_utils.get_column_name('database', 'schema', None, 'column')
+        database_utils.get_column_name('schema', None, 'column', 'database')
 
 
 def test_get_column_name_with_invalid_column():
@@ -92,4 +104,4 @@ def test_get_column_name_with_invalid_column():
     """
 
     with pytest.raises(CoreValueError):
-        database_utils.get_column_name('database', 'schema', 'table', '')
+        database_utils.get_column_name('schema', 'table', '', 'database')
