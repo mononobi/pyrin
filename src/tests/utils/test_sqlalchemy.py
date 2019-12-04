7cee122416039e01153c459da96c69d1116d861c
mono <mohamadnobakht@gmail.com>
2019-12-04 14:51:19 +0330

    1. test_sqlalchemy tests for entity_to_dict_list() method implemented.

--- src/tests/utils/test_sqlalchemy.py
+++ src/tests/utils/test_sqlalchemy.py
@@ -106,6 +106,108 @@ def test_dict_to_entity_with_none_dict():
     assert entity.age is None
 
 
+def test_entity_to_dict_list():
+    """
+    converts the given list of entities into a list of dicts.
+    """
+
+    entity1 = SampleEntity()
+    entity1.id = 1000
+    entity1.name = 'name1'
+    entity1.age = 10
+
+    entity2 = SampleEntity()
+    entity2.id = 2000
+    entity2.name = 'name2'
+    entity2.age = 20
+
+    entity3 = SampleEntity()
+    entity3.id = 3000
+    entity3.name = 'name3'
+    entity3.age = 30
+
+    entities = []
+    entities.append(entity1)
+    entities.append(entity2)
+    entities.append(entity3)
+
+    result = sqlalchemy_utils.entity_to_dict_list(entities)
+
+    assert isinstance(result, list)
+    assert len(result) == 3
+
+    dict1 = result[0]
+    dict2 = result[1]
+    dict3 = result[2]
+
+    assert isinstance(dict1, dict)
+    assert isinstance(dict2, dict)
+    assert isinstance(dict3, dict)
+
+    assert dict1.get('id', None) == 1000
+    assert dict2.get('id', None) == 2000
+    assert dict3.get('id', None) == 3000
+
+    assert dict1.get('name', None) == 'name1'
+    assert dict2.get('name', None) == 'name2'
+    assert dict3.get('name', None) == 'name3'
+
+    assert dict1.get('age', None) == 10
+    assert dict2.get('age', None) == 20
+    assert dict3.get('age', None) == 30
+
+
+def test_entity_to_dict_list_with_single_item():
+    """
+    converts the given list of single entity into a list of a single dict.
+    """
+
+    entity1 = SampleEntity()
+    entity1.id = 1000
+    entity1.name = 'name1'
+    entity1.age = 10
+
+    entities = []
+    entities.append(entity1)
+
+    result = sqlalchemy_utils.entity_to_dict_list(entities)
+
+    assert isinstance(result, list)
+    assert len(result) == 1
+
+    dict1 = result[0]
+
+    assert isinstance(dict1, dict)
+
+    assert dict1.get('id', None) == 1000
+    assert dict1.get('name', None) == 'name1'
+    assert dict1.get('age', None) == 10
+
+
+def test_entity_to_dict_list_with_empty_list():
+    """
+    converts the given empty list into a list of a dicts.
+    it should return an empty list.
+    """
+
+    result = sqlalchemy_utils.entity_to_dict_list([])
+
+    assert isinstance(result, list)
+    assert len(result) == 0
+
+
+def test_entity_to_dict_list_with_none():
+    """
+    converts the given None value into a list of a dicts.
+    it should return an empty list.
+    """
+
+    result = sqlalchemy_utils.entity_to_dict_list(None)
+
+    assert isinstance(result, list)
+    assert len(result) == 0
+
+
 def test_like_both():
     """
     gets a copy of string with `%` attached to both
