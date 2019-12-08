8ff829010768defa4281392a61d76112c3baf7bb
mono <mohamadnobakht@gmail.com>
2019-12-08 16:36:44 +0330

    1. CoreQuery init keyword changed from limit to bound.

--- src/pyrin/database/orm/query/base.py
+++ src/pyrin/database/orm/query/base.py
@@ -30,7 +30,7 @@ class CoreQuery(Query):
         :param tuple entities: entities or columns that are needed for query.
         :param Session session: optional session object to bind this query to it.
 
-        :keyword Union[type, tuple[type]] limit: class type of the entities that this
+        :keyword Union[type, tuple[type]] bound: class type of the entities that this
                                                  query instance will work on. if the
                                                  query is working on multiple entities,
                                                  this value must be a tuple of all class
@@ -38,30 +38,30 @@ class CoreQuery(Query):
 
                                                  for example: if you set
                                                  `entities=SomeEntity.id, AnotherEntity.name`
-                                                 you should leave `limit=None` to skip validation
+                                                 you should leave `bound=None` to skip validation
                                                  or you could set
-                                                 `limit=(SomeEntity, AnotherEntity)`
+                                                 `bound=(SomeEntity, AnotherEntity)`
                                                  this way validation succeeds, but if
-                                                 you set `limit=SomeEntity`
+                                                 you set `bound=SomeEntity`
                                                  then the query will not be executed
                                                  and an error would be raised.
 
         :raises DataCouldNotBeFetchedError: data could not be fetched error.
         """
 
-        limit = options.get('limit', None)
-        if limit is not None:
-            self._prepare_validation(entities, limit)
+        bound = options.get('bound', None)
+        if bound is not None:
+            self._prepare_validation(entities, bound)
 
         super(CoreQuery, self).__init__(entities, session)
 
-    def _validate_limit(self, entities, limit):
+    def _validate_bound(self, entities, bound):
         """
-        validates the query for given entities based on provided limit.
+        validates the query for given entities based on provided bound.
 
         :param tuple entities: entities or columns that needed for query.
 
-        :param Union[type, tuple[type]] limit: class type of the entities that this
+        :param Union[type, tuple[type]] bound: class type of the entities that this
                                                query instance will work on. if the
                                                query is working on multiple entities,
                                                this value must be a tuple of all class
@@ -70,7 +70,7 @@ class CoreQuery(Query):
         :raises DataCouldNotBeFetchedError: data could not be fetched error.
         """
 
-        limit_classes = set(entity for entity in limit if inspect.isclass(entity)
+        bound_classes = set(entity for entity in bound if inspect.isclass(entity)
                             and issubclass(entity, CoreEntity))
 
         requested_classes = set(entity for entity in entities if inspect.isclass(entity)
@@ -83,17 +83,17 @@ class CoreQuery(Query):
 
         all_requested_classes = requested_classes.union(requested_classes_by_column)
 
-        if not all_requested_classes.issubset(limit_classes):
+        if not all_requested_classes.issubset(bound_classes):
             raise DataCouldNotBeFetchedError(_('Requested data could not be fetched. '
                                                'Please revise the requested fields.'))
 
-    def _prepare_validation(self, entities, limit):
+    def _prepare_validation(self, entities, bound):
         """
-        prepares entities and limit and validates them if required.
+        prepares entities and bound and validates them if required.
 
         :param tuple entities: entities or columns that needed for query.
 
-        :param Union[type, tuple[type]] limit: class type of the entities that this
+        :param Union[type, tuple[type]] bound: class type of the entities that this
                                                query instance will work on. if the
                                                query is working on multiple entities,
                                                this value must be a tuple of all class
@@ -102,16 +102,16 @@ class CoreQuery(Query):
         :raises DataCouldNotBeFetchedError: data could not be fetched error.
         """
 
-        if limit is None or entities is None:
+        if bound is None or entities is None:
             return
 
-        if not isinstance(limit, LIST_TYPES):
-            limit = tuple(limit)
+        if not isinstance(bound, LIST_TYPES):
+            bound = tuple(bound)
 
         if not isinstance(entities, LIST_TYPES):
             entities = tuple(entities)
 
-        if len(limit) <= 0 or len(entities) <= 0:
+        if len(bound) <= 0 or len(entities) <= 0:
             return
 
-        self._validate_limit(entities, limit)
+        self._validate_bound(entities, bound)
