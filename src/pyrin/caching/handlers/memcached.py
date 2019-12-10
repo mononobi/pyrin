b8f70bad5dcec720cfa6e81312aa5de20f5c8ae9
mono <mohamadnobakht@gmail.com>
2019-12-10 12:54:06 +0330

    1. memcached added.

---
+++ src/pyrin/caching/handlers/memcached.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+caching handlers memcached module.
+"""
+
+import pyrin.configuration.services as config_services
+
+from pyrin.caching.handlers.base import CachingHandlerBase
+
+
+class MemcachedCachingHandler(CachingHandlerBase):
+    """
+    memcached caching handler class.
+    """
+
+    def __init__(self, **options):
+        """
+        initializes an instance of MemcachedCachingHandler.
+        """
+
+        options.update(timeout=config_services.get('caching', 'memcached',
+                                                   'memcached_timeout'))
+
+        CachingHandlerBase.__init__(self, 'memcached', **options)
