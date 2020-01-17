54982ff4013e5339b29d4d06eb58aad73a3255cd
mono <mohamadnobakht@gmail.com>
2020-01-18 02:37:44 +0330

    1. database.hooks.after_packages_loaded() modified to create all models in database on server startup if required.

--- src/pyrin/database/hooks.py
+++ src/pyrin/database/hooks.py
@@ -4,6 +4,7 @@ database hooks module.
 """
 
 import pyrin.database.services as database_services
+import pyrin.configuration.services as config_services
 
 from pyrin.packaging.decorators import packaging_hook
 from pyrin.packaging.hooks import PackagingHookBase
@@ -30,3 +31,8 @@ class PackagingHook(PackagingHookBase):
         # we have to configure session factories after all models have
         # been loaded to enable multiple database connections if needed.
         database_services.configure_session_factories()
+
+        # we should check whether it is required to create
+        # all entities in database on server startup.
+        if config_services.get('database', 'general', 'create_on_startup') is True:
+            database_services.create_all()
