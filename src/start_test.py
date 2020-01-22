16c4838c1cad38f4b3b383519e29490f8049dbd6
mono <mohamadnobakht@gmail.com>
2020-01-22 03:34:31 +0330

    1. start_test.py script modified.

--- src/start_test.py
+++ src/start_test.py
@@ -13,24 +13,43 @@ from pyrin.utils.path import resolve_application_root_path
 from tests import PyrinTestApplication
 
 
+def remove(name):
+    """
+    removes a file or directory by specified name from application root directory.
+
+    :param str name: name of the file or directory to be removed.
+    """
+
+    root_path = resolve_application_root_path()
+    file_path = os.path.join(root_path, name)
+    command = 'rm -r {path}'.format(path=file_path)
+    os.system(command)
+
+
 def cleanup():
     """
     cleanups the environment after running all tests.
     """
 
     drop_schema()
-    remove_cache()
+    remove_coverage()
+    remove_pytest_cache()
 
 
-def remove_cache():
+def remove_pytest_cache():
     """
     removes pytest cache directory.
     """
 
-    root_path = resolve_application_root_path()
-    cache_path = os.path.join(root_path, '.pytest_cache')
-    command = 'rm -r {path}'.format(path=cache_path)
-    os.system(command)
+    remove('.pytest_cache')
+
+
+def remove_coverage():
+    """
+    removes coverage file.
+    """
+
+    remove('.coverage')
 
 
 def drop_schema():
@@ -48,5 +67,6 @@ def drop_schema():
 # on windows works as expected.
 if __name__ == '__main__':
     app = PyrinTestApplication()
-    pytest.main()
+    pytest.main(['--cov-config=tests/settings/pytest.coverage.config',
+                 '--cov=pyrin'])
     cleanup()
