445364eeb6f063eacc730e56b9d29a6b017b409c
mono <mohamadnobakht@gmail.com>
2020-02-13 04:59:12 +0330

    1. default settings added to pyrin.

---
+++ src/pyrin/settings/default/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+settings default package.
+every project must have a settings folder inside its main package.
+but if there is no such folder, pyrin will use these default settings.
+"""
