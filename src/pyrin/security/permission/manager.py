38deceaf2f34e7621811473231bb7ca021f5b6f9
mono <mohamadnobakht@gmail.com>
2020-02-27 03:30:41 +0330

    Extend pyrin PermissionManager module

--- src/pyrin/security/permission/manager.py
+++ src/pyrin/security/permission/manager.py
@@ -4,7 +4,10 @@ permission manager module.
 """
 
 from pyrin.core.context import Context, Manager
+from pyrin.core.exceptions import CoreNotImplementedError
+from pyrin.database.services import get_current_store
 from pyrin.security.permission.base import PermissionBase
+from pyrin.utils.sqlalchemy import entity_to_dict_list
 from pyrin.security.permission.exceptions import InvalidPermissionTypeError, \
     DuplicatedPermissionError
 
@@ -12,8 +15,6 @@ from pyrin.security.permission.exceptions import InvalidPermissionTypeError, \
 class PermissionManager(Manager):
     """
     permission manager class.
-    this class is intended to be an interface for top level application's permission
-    package, so most methods of this class will raise CoreNotImplementedError.
     """
 
     def __init__(self):
@@ -63,7 +64,57 @@ class PermissionManager(Manager):
         """
         synchronizes all permissions with database.
         it creates or updates the available permissions.
-        if you don't need permissions in your application
-        leave this method not implemented.
         """
-        pass
+
+        entities = [permission.to_entity() for permission in self.get_permissions()]
+        needs_update = [entity for entity in entities if
+                        self._exists(*entity.primary_key(as_tuple=True)) is True]
+        needs_insert = list(set(entities).difference(set(needs_update)))
+
+        if needs_insert:
+            self._bulk_insert(needs_insert)
+        if needs_update:
+            self._bulk_update(needs_update)
+
+    def _exists(self, *primary_key):
+        """
+        gets a value indicating that given permission exists in database.
+
+        this method must be implemented in subclasses, the input value
+        could be as many as needed arguments to represent the primary key
+        of your permission entity.
+        if you don't want to use permissions concept in your application,
+        you could leave this method unimplemented.
+
+        :param object primary_key: permission primary key value.
+
+        :raises CoreNotImplementedError: core not implemented error.
+
+        :rtype: bool
+        """
+
+        raise CoreNotImplementedError()
+
+    def _bulk_insert(self, entities):
+        """
+        bulk inserts the given permission entities.
+
+        :param list[CoreEntity] entities: permission entities to be inserted.
+        """
+
+        if len(entities) > 0:
+            store = get_current_store()
+            store.bulk_insert_mappings(type(entities[0]), entity_to_dict_list(entities, False))
+            store.commit()
+
+    def _bulk_update(self, entities):
+        """
+        bulk updates the given permission entities.
+
+        :param list[CoreEntity] entities: permission entities to be updated.
+        """
+
+        if len(entities) > 0:
+            store = get_current_store()
+            store.bulk_update_mappings(type(entities[0]), entity_to_dict_list(entities, False))
+            store.commit()
