090797617ecb4bf25736aa2f1ca7f154e2cf8667
mono <mohamadnobakht@gmail.com>
2020-03-11 06:09:27 +0330

    Revise cli.core.command

--- src/pyrin/cli/core/command.py
+++ src/pyrin/cli/core/command.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-cli core cli module.
+cli core command module.
 """
 
-import os
 import sys
-
-
-python_path = os.path.abspath('../../..')
-os.environ['PYTHONPATH'] = python_path
-sys.path.append(python_path)
-
 
 import pyrin.cli.core.services as core_services
 
 from pyrin.utils.custom_print import print_error
 
 
-if __name__ == '__main__':
+def main():
     try:
         handler_name = sys.argv[1]
         core_services.create(handler_name)
