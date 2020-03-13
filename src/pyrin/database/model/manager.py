2d36b8d39c074fa5edf33fb4a0045f3a3cd36f95
mono <mohamadnobakht@gmail.com>
2020-03-13 19:11:07 +0330

    Enhance database.model services

--- src/pyrin/database/model/manager.py
+++ src/pyrin/database/model/manager.py
@@ -14,24 +14,26 @@ class ModelManager(Manager):
     model manager class.
     """
 
-    def is_core_entity(self, value):
+    def is_base_entity(self, value):
         """
-        gets a value indicating that input value is an instance of `CoreEntity`.
+        gets a value indicating that input value is an instance of base entity type.
 
-        this method could be used where direct reference to `CoreEntity`
-        produces an error.
+        base entity type by default is `CoreEntity`.
+        this method could be used where direct reference to base entity
+        type produces an error on server startup.
 
         :param object value: value to be checked.
 
         :rtype: bool
         """
 
-        return isinstance(value, CoreEntity)
+        return isinstance(value, self.get_base_entity_type())
 
-    def is_abstract_keyed_tuple(self, value):
+    def is_base_keyed_tuple(self, value):
         """
-        gets a value indicating that input value is an instance of `AbstractKeyedTuple`.
+        gets a value indicating that input value is an instance of base keyed tuple type.
 
+        base keyed tuple type by default is `AbstractKeyedTuple`.
         `AbstractKeyedTuple` objects are those objects that are returned by sqlalchemy
         `Query` with columns or multiple entities.
 
@@ -40,4 +42,30 @@ class ModelManager(Manager):
         :rtype: bool
         """
 
-        return isinstance(value, AbstractKeyedTuple)
+        return isinstance(value, self.get_base_keyed_tuple_type())
+
+    def get_base_entity_type(self):
+        """
+        gets the application's base entity type.
+
+        base entity type by default is `CoreEntity`.
+        this method could be used where direct reference to base entity
+        type produces an error on server startup.
+
+        :rtype: type
+        """
+
+        return CoreEntity
+
+    def get_base_keyed_tuple_type(self):
+        """
+        gets the application's base keyed tuple type.
+
+        base keyed tuple type by default is `AbstractKeyedTuple`.
+        `AbstractKeyedTuple` objects are those objects that are returned by sqlalchemy
+        `Query` with columns or multiple entities.
+
+        :rtype: type
+        """
+
+        return AbstractKeyedTuple
--- src/pyrin/database/model/services.py
+++ src/pyrin/database/model/services.py
@@ -7,25 +7,27 @@ from pyrin.application.services import get_component
 from pyrin.database.model import ModelPackage
 
 
-def is_core_entity(value):
+def is_base_entity(value):
     """
-    gets a value indicating that input value is an instance of `CoreEntity`.
+    gets a value indicating that input value is an instance of base entity type.
 
-    this method could be used where direct reference to `CoreEntity`
-    produces an error.
+    base entity type by default is `CoreEntity`.
+    this method could be used where direct reference to base entity
+    type produces an error on server startup.
 
     :param object value: value to be checked.
 
     :rtype: bool
     """
 
-    return get_component(ModelPackage.COMPONENT_NAME).is_core_entity(value)
+    return get_component(ModelPackage.COMPONENT_NAME).is_base_entity(value)
 
 
-def is_abstract_keyed_tuple(value):
+def is_base_keyed_tuple(value):
     """
-    gets a value indicating that input value is an instance of `AbstractKeyedTuple`.
+    gets a value indicating that input value is an instance of base keyed tuple type.
 
+    base keyed tuple type by default is `AbstractKeyedTuple`.
     `AbstractKeyedTuple` objects are those objects that are returned by sqlalchemy
     `Query` with columns or multiple entities.
 
@@ -34,4 +36,32 @@ def is_abstract_keyed_tuple(value):
     :rtype: bool
     """
 
-    return get_component(ModelPackage.COMPONENT_NAME).is_abstract_keyed_tuple(value)
+    return get_component(ModelPackage.COMPONENT_NAME).is_base_keyed_tuple(value)
+
+
+def get_base_entity_type():
+    """
+    gets the application's base entity type.
+
+    base entity type by default is `CoreEntity`.
+    this method could be used where direct reference to base entity
+    type produces an error on server startup.
+
+    :rtype: type
+    """
+
+    return get_component(ModelPackage.COMPONENT_NAME).get_base_entity_type()
+
+
+def get_base_keyed_tuple_type():
+    """
+    gets the application's base keyed tuple type.
+
+    base keyed tuple type by default is `AbstractKeyedTuple`.
+    `AbstractKeyedTuple` objects are those objects that are returned by sqlalchemy
+    `Query` with columns or multiple entities.
+
+    :rtype: type
+    """
+
+    return get_component(ModelPackage.COMPONENT_NAME).get_base_keyed_tuple_type()
