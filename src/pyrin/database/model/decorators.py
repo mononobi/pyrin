59f4691e551a19df17474d1ffce77a0921823535
mono <mohamadnobakht@gmail.com>
2020-03-18 19:26:56 +0330

    Move bind decorator to model package

---
+++ src/pyrin/database/model/decorators.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+model decorators module.
+"""
+
+import pyrin.database.services as database_services
+
+
+def bind(name, **options):
+    """
+    decorator to bind a model class to a database.
+
+    :param str name: bind name to associate with the model.
+
+    :raises InvalidEntityTypeError: invalid entity type error.
+
+    :returns: model class.
+    :rtype: type
+    """
+
+    def decorator(cls):
+        """
+        decorates the given model class and binds it with the specified database.
+
+        :param type cls: model class.
+
+        :returns: model class.
+        :rtype: type
+        """
+
+        database_services.register_bind(cls, name, **options)
+
+        return cls
+
+    return decorator
