665dc277752e761d143eb30bd13c0e3d0b511eb5
mono <mohamadnobakht@gmail.com>
2020-03-24 02:10:33 +0430

    Extend manage.py script

--- src/manage.py
+++ src/manage.py
@@ -7,12 +7,16 @@ to enable locale management for application, execute:
 
 to enable migrations for application, execute:
 `python manage.py alembic enable`
+
+to create a new package for application, execute:
+`python manage.py template package`
 """
 
 import fire
 
 from pyrin.database.migration.alembic.cli import AlembicCLI
 from pyrin.globalization.locale.babel.cli import BabelCLI
+from pyrin.template.cli import TemplateCLI
 from pyrin.utils.environment import set_python_path
 
 set_python_path('.')
@@ -33,6 +37,7 @@ class Groups(object):
 
     `python manage.py alembic upgrade --arg value`
     `python manage.py babel extract --arg value`
+    `python manage.py template package`
     """
 
     def __init__(self):
@@ -42,6 +47,7 @@ class Groups(object):
 
         self.babel = BabelCLI()
         self.alembic = AlembicCLI()
+        self.template = TemplateCLI()
 
 
 if __name__ == '__main__':
