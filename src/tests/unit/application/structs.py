bcc38fa353a7c4192b6811c7ce81cebcb2584b75
mono <mohamadnobakht@gmail.com>
2020-06-30 01:02:16 +0430

    Revise CoreRequestMock class

--- src/tests/unit/application/structs.py
+++ src/tests/unit/application/structs.py
@@ -3,13 +3,10 @@
 application structs module.
 """
 
-import pyrin.utils.unique_id as uuid_utils
-import pyrin.globalization.datetime.services as datetime_services
-
 from pyrin.application.base import Application
 from pyrin.application.structs import Component
-from pyrin.core.structs import CoreObject, Context, Manager
-from pyrin.settings.static import APPLICATION_ENCODING, DEFAULT_COMPONENT_KEY
+from pyrin.core.structs import Manager
+from pyrin.processor.request.base import CoreRequest
 
 
 class ApplicationMock(Application):
@@ -19,34 +16,13 @@ class ApplicationMock(Application):
     pass
 
 
-class CoreRequestMock(CoreObject):
+class CoreRequestMock(CoreRequest):
     """
     core request mock class.
     """
 
-    # charset of the request.
-    charset = APPLICATION_ENCODING
-
     def __init__(self):
-        super().__init__()
-
-        self.request_id = uuid_utils.generate_uuid4()
-        self.request_date = datetime_services.now()
-        self.user = None
-        self.component_custom_key = DEFAULT_COMPONENT_KEY
-        self.context = Context()
-        self.headers = Context()
-
-    def __str__(self):
-        result = 'request id: "{request_id}", request date: "{request_date}", ' \
-                 'user: "{user}", component_custom_key: "{component}"'
-        return result.format(request_id=self.request_id,
-                             request_date=self.request_date,
-                             user=self.user,
-                             component=self.component_custom_key)
-
-    def __hash__(self):
-        return hash(self.request_id)
+        super().__init__({}, populate_request=True)
 
 
 class OnlyManagerMock(Manager):
