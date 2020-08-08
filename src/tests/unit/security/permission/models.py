e6fa9b24179941b7ed8782c6d8e718a960111f92
mono <mohamadnobakht@gmail.com>
2020-08-09 01:24:52 +0430

    Revise tests PermissionEntity

--- src/tests/unit/security/permission/models.py
+++ src/tests/unit/security/permission/models.py
@@ -5,7 +5,6 @@ permission models module.
 
 from sqlalchemy import Unicode, SmallInteger
 
-from pyrin.core.structs import DTO
 from pyrin.database.model.base import CoreEntity
 from pyrin.database.orm.sql.schema.base import CoreColumn
 
@@ -15,7 +14,7 @@ class PermissionBaseEntity(CoreEntity):
     permission base entity class.
     """
 
-    __tablename__ = 'permission'
+    _table = 'permission'
 
     id = CoreColumn(name='id', type_=SmallInteger, primary_key=True, nullable=False, index=True)
 
@@ -25,6 +24,6 @@ class PermissionEntity(PermissionBaseEntity):
     permission entity class.
     """
 
-    __table_args__ = DTO(extend_existing=True)
+    _extend_existing = True
 
     description = CoreColumn(name='description', type_=Unicode(100), nullable=False)
