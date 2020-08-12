4bf201936db5883fc8d5087e7f9cf629aac17836
mono <mohamadnobakht@gmail.com>
2020-08-13 01:16:27 +0430

    Enhance logging masking package

--- src/pyrin/logging/masking/manager.py
+++ src/pyrin/logging/masking/manager.py
@@ -14,19 +14,73 @@ class LoggingMaskingManager(Manager):
     logging masking manager class.
     """
 
-    # theses are default keywords that will be masked if they are present in a dict.
+    # these are default keywords that will be masked if they are present in a dict.
     # these values could be extended or disabled in 'logging.masking' config store.
-    DEFAULT_MASKED_KEYWORDS = {'password', 'passwd', 'pass', 'email', 'email_address',
-                               'token', 'auth_token', 'authtoken', 'api_key', 'apikey',
-                               'secret_key', 'secret', 'credit_card', 'credit_card_no',
-                               'credit_card_number', 'card_number', 'card_no', 'phone',
-                               'phone_number', 'phone_no', 'mobile', 'mobile_no',
-                               'mobile_number', 'address', 'postal_address', 'postal_code',
-                               'zip_code', 'national_code', 'national_no', 'national_number',
-                               'identity_code', 'identity_no', 'identity_number', 'birth_date',
-                               'birthdate', 'authentication', 'authorization', 'home_address',
-                               'birth_certificate', 'birth_certificate_no', 'passport_number',
-                               'birth_certificate_number', 'passport', 'passport_no'}
+    # note that all values will be normalized and matched against also normalized values.
+    # for example 'api_key' and 'APIkey' and 'api-key' will be considered equal.
+    DEFAULT_MASKED_KEYWORDS = {'address',
+                               'api_key',
+                               'api_token',
+                               'auth_token',
+                               'authentication',
+                               'authorization',
+                               'birth_certificate',
+                               'birth_certificate_no',
+                               'birth_certificate_number',
+                               'birth_date',
+                               'birthday',
+                               'card_no',
+                               'card_number',
+                               'certificate',
+                               'credential',
+                               'credentials',
+                               'credit_card',
+                               'credit_card_no',
+                               'credit_card_number',
+                               'cvv',
+                               'debit_card',
+                               'debit_card_no',
+                               'debit_card_number',
+                               'email',
+                               'email_address',
+                               'fingerprint',
+                               'hash',
+                               'hash_value',
+                               'home_address',
+                               'http_authorization',
+                               'identity_code',
+                               'identity_no',
+                               'identity_number',
+                               'mobile',
+                               'mobile_no',
+                               'mobile_number',
+                               'national_code',
+                               'national_no',
+                               'national_number',
+                               'oauth',
+                               'oauth_token',
+                               'pass',
+                               'passport',
+                               'passport_no',
+                               'passport_number',
+                               'passwd',
+                               'password',
+                               'phone',
+                               'phone_no',
+                               'phone_number',
+                               'postal_address',
+                               'postal_code',
+                               'private_key',
+                               'rsa',
+                               'rsa_key',
+                               'secret',
+                               'secret_key',
+                               'token',
+                               'www-authenticate',
+                               'zip_code'}
+
+    # a class type to be used as masked dict.
+    masked_dict_class = MaskedDict
 
     def __init__(self):
         """
@@ -35,42 +89,61 @@ class LoggingMaskingManager(Manager):
 
         super().__init__()
 
-        self._masks = self._load_masks()
+        self._masks = self._get_masked_keywords()
 
-    def _load_masks(self):
+    def _get_masked_keywords(self):
         """
         gets all keywords that should be masked.
 
         :rtype: set[str]
         """
 
-        normalized_defaults = self._normalize_case(self.DEFAULT_MASKED_KEYWORDS)
+        normalized_defaults = self._normalize_items(self.DEFAULT_MASKED_KEYWORDS)
         include_defaults = config_services.get_active('logging.masking',
                                                       'include_default_keywords')
         extra_keywords = config_services.get_active('logging.masking', 'extra_keywords')
-        extra_keywords = self._normalize_case(extra_keywords)
+        extra_keywords = self._normalize_items(extra_keywords)
 
-        results = []
+        results = list(extra_keywords)
         if include_defaults is True:
             results.extend(normalized_defaults)
 
-        results.extend(extra_keywords)
         return set(results)
 
-    def _normalize_case(self, items):
+    def _normalize_items(self, items):
         """
-        makes all items lowercase and returns a new list.
+        normalizes all given items and returns a new list.
 
-        :param list[str] | tuple[str] | set[str] items: items to be made lower case.
+        :param list[str] | tuple[str] | set[str] items: items to be normalized.
 
         :rtype: list[str]
         """
 
         results = []
         for item in items:
-            results.append(item.lower())
+            results.append(self._normalize(item))
 
         return results
+
+    def _normalize(self, value):
+        """
+        normalizes given value.
+
+        if the value is not a string, it returns the value itself.
+        this method does not check `isinstance` to detect strings, because it
+        has a significant performance hit. so it returns the same value
+        whenever an `AttributeError` is occurred.
+
+        :param str | object value: value to be normalized.
+
+        :rtype: str | object
+        """
+
+        try:
+            return value.lower().replace('_', '').replace('-', '') \
+                .replace('.', '').replace(' ', '')
+        except AttributeError:
+            return value
 
     def should_mask(self, key):
         """
@@ -81,10 +154,7 @@ class LoggingMaskingManager(Manager):
         :rtype: bool
         """
 
-        if not isinstance(key, str):
-            return False
-
-        return key.lower() in self._masks
+        return self._normalize(key) in self._masks
 
     def mask(self, data, **options):
         """
@@ -98,7 +168,7 @@ class LoggingMaskingManager(Manager):
         :rtype: MaskedDict | object
         """
 
-        if isinstance(data, dict):
-            return MaskedDict(data)
+        if isinstance(data, dict) and not isinstance(data, self.masked_dict_class):
+            return self.masked_dict_class(data)
 
         return data
