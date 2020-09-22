76c01ded0c92d31d3d834f7aa26c97bc90906039
mono <mohamadnobakht@gmail.com>
2020-09-22 18:58:36 +0330

    Revise manage_unit.py script

--- src/manage_unit.py
+++ src/manage_unit.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-manage_unit module.
+manage unit module.
 
 to enable locale management for unit tests, execute:
 `python manage.py babel enable`
@@ -10,13 +10,18 @@ to enable migrations for unit tests, execute:
 
 to create a new package for unit tests, execute:
 `python manage.py template package`
+
+usage example:
+
+`python manage.py alembic upgrade --arg value`
+`python manage.py babel extract --arg value`
+`python manage.py template package`
+`python manage.py celery worker --arg value`
 """
 
 import fire
 
-from pyrin.database.migration.alembic.cli import AlembicCLI
-from pyrin.globalization.locale.babel.cli import BabelCLI
-from pyrin.template.cli import TemplateCLI
+from pyrin.cli.services import get_cli_groups
 
 from tests.unit import PyrinUnitTestApplication
 
@@ -25,28 +30,5 @@ app_instance = PyrinUnitTestApplication(import_name='tests.unit',
                                         scripting_mode=True)
 
 
-class Groups(object):
-    """
-    groups class.
-
-    each cli handler group is resided in its relevant group name.
-
-    usage example:
-
-    `python manage.py alembic upgrade --arg value`
-    `python manage.py babel extract --arg value`
-    `python manage.py template package`
-    """
-
-    def __init__(self):
-        """
-        initializes an instance of Groups.
-        """
-
-        self.babel = BabelCLI()
-        self.alembic = AlembicCLI()
-        self.template = TemplateCLI()
-
-
 if __name__ == '__main__':
-    fire.Fire(Groups())
+    fire.Fire(get_cli_groups())
