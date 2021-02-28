5a237498a021ddd5b22189de6b59882bd0aa3ff5
mono <mohamadnobakht@gmail.com>
2021-03-01 00:33:41 +0330

    Revise tests

--- src/tests/unit/database/migration/test_services.py
+++ src/tests/unit/database/migration/test_services.py
@@ -8,19 +8,6 @@ import pytest
 import pyrin.database.migration.services as migration_services
 
 
-def test_get_engine_to_table_map():
-    """
-    gets engine to table map dictionary.
-    """
-    pass
-    # engine_to_table_map = extended_database_services.get_engine_to_table_map()
-    # all_engines = extended_database_services.get_all_engines()
-    #
-    # assert engine_to_table_map is not None
-    # assert len(engine_to_table_map) >= 3
-    # assert all(engine in engine_to_table_map for engine in all_engines)
-
-
 @pytest.mark.skip('we should not create database between '
                   'tests, server has created it on startup.')
 def test_create_all():
