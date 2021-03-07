dcc914d52602318d0550fa42aaf27d7452a7f97f
mono <mohamadnobakht@gmail.com>
2021-03-07 14:31:10 +0330

    Implement Constant class

--- src/pyrin/core/globals.py
+++ src/pyrin/core/globals.py
@@ -7,8 +7,46 @@ from flask_babel import gettext
 from sqlalchemy.util._collections import AbstractKeyedTuple
 
 
+class Constant:
+    """
+    constant class.
+
+    this is a helper class for defining global constant objects.
+    we have to declare this class in `globals` instead of `structs`
+    to prevent circular dependency.
+    """
+
+    def __init__(self, name, **options):
+        """
+        initializes an instance of Constant.
+
+        :param str name: this constant's name.
+        """
+
+        super().__init__()
+        self._name = name
+
+    def __repr__(self):
+        """
+        gets the string representation of current constant.
+
+        :rtype: str
+        """
+
+        return str(self)
+
+    def __str__(self):
+        """
+        gets the string representation of current constant.
+
+        :rtype: str
+        """
+
+        return self._name
+
+
 # this value should be used as `None`, where the `None` itself has a meaning.
-NULL = object()
+NULL = Constant('NULL')
 
 # this value should be used where we need to reference class type of None objects.
 NONE_TYPE = type(None)
@@ -24,8 +62,8 @@ ROW_RESULT = AbstractKeyedTuple
 
 # this value should be used instead of True when we want to preserve security of
 # inputs which come from client to prevent injection of invalid values through options.
-SECURE_TRUE = object()
+SECURE_TRUE = Constant('SECURE_TRUE')
 
 # this value should be used instead of False when we want to preserve security of
 # inputs which come from client to prevent injection of invalid values through options.
-SECURE_FALSE = object()
+SECURE_FALSE = Constant('SECURE_FALSE')
