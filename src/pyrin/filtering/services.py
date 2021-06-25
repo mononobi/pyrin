9993ae9d745fe2c354c1105846bb8fe369ae649b
mono <mohamadnobakht@gmail.com>
2021-06-25 23:33:12 +0430

    Revise filter service

--- src/pyrin/filtering/services.py
+++ src/pyrin/filtering/services.py
@@ -7,12 +7,16 @@ from pyrin.application.services import get_component
 from pyrin.filtering import FilteringPackage
 
 
-def filter(entity, *ignore, **filters):
+def filter(entity, filters, *ignore):
     """
     gets filtering expressions for given entity type based on given filters.
 
+    it removes all keys that are applied as filter from filters input.
+
     :param type[pyrin.database.model.base.BaseEntity] entity: entity class type to
                                                               be used for filtering.
+
+    :param dict filters: filters to be applied.
 
     :param sqlalchemy.orm.attributes.InstrumentedAttribute ignore: columns to be ignored
                                                                    from filtering.
@@ -21,4 +25,4 @@ def filter(entity, *ignore, **filters):
     :rtype: list
     """
 
-    return get_component(FilteringPackage.COMPONENT_NAME).filter(entity, *ignore, **filters)
+    return get_component(FilteringPackage.COMPONENT_NAME).filter(entity, filters, *ignore)
