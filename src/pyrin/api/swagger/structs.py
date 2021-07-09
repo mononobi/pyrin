4c94dd5143accb148ba060ee7069f71e4a133a76
mono <mohamadnobakht@gmail.com>
2021-07-10 02:31:55 +0430

    Override default configs

--- src/pyrin/api/swagger/structs.py
+++ src/pyrin/api/swagger/structs.py
@@ -37,6 +37,23 @@ class ExtendedSwagger(Swagger):
     schema attributes into registered specs.
     """
 
+    DEFAULT_CONFIG = {
+        "headers": [
+        ],
+        "specs": [
+            {
+                "endpoint": 'swagger',
+                "route": '/swagger.json',
+                "rule_filter": lambda rule: True,  # all in
+                "model_filter": lambda tag: True,  # all in
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        # "static_folder": "static",  # must be set by user
+        "swagger_ui": True,
+        "specs_route": "/swagger/"
+    }
+
     def _get_parameters_section(self, swag):
         """
         gets parameters section from given swag info.
