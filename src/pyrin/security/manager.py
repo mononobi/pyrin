9ea7cfb8a623e7e8336f3fd09e3ef38e243132d7
mono <mohamadnobakht@gmail.com>
2021-10-29 03:08:06 +0330

    Remove extra methods

--- src/pyrin/security/manager.py
+++ src/pyrin/security/manager.py
@@ -3,14 +3,11 @@
 security manager module.
 """
 
-from abc import abstractmethod
-
 import pyrin.security.encryption.services as encryption_services
 import pyrin.security.hashing.services as hashing_services
 
 from pyrin.core.globals import _
 from pyrin.core.structs import Manager
-from pyrin.core.exceptions import CoreNotImplementedError
 from pyrin.security import SecurityPackage
 from pyrin.security.exceptions import InvalidPasswordLengthError, \
     InvalidEncryptionTextLengthError
@@ -19,9 +16,6 @@ from pyrin.security.exceptions import InvalidPasswordLengthError, \
 class SecurityManager(Manager):
     """
     security manager class.
-
-    this class is intended to provide some services needed in pyrin application.
-    the top level application must extend this class considering business requirements.
     """
 
     package_class = SecurityPackage
@@ -66,18 +60,3 @@ class SecurityManager(Manager):
             raise InvalidEncryptionTextLengthError(_('Input text has invalid length.'))
 
         return encryption_services.encrypt(text)
-
-    @abstractmethod
-    def has_permission(self, user, permissions, **options):
-        """
-        gets a value indicating that given user has the specified permissions.
-
-        :param user: user identity to check its permissions.
-        :param list[PermissionBase] permissions: permissions to check for user.
-
-        :raises CoreNotImplementedError: core not implemented error.
-
-        :rtype: bool
-        """
-
-        raise CoreNotImplementedError()
