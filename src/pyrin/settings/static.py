be7cc41370002b546cc1451c71367c956927ff30
mono <mohamadnobakht@gmail.com>
2022-06-07 16:22:42 +0200

    Revise comments

--- src/pyrin/settings/static.py
+++ src/pyrin/settings/static.py
@@ -13,7 +13,7 @@ APPLICATION_ENCODING = 'utf-8'
 
 # this value will be used to register default components that does not belong
 # to any custom implementation. any other custom implementation that needs to be
-# exposed through services, should provide it's own relevant key.
+# exposed through services, should provide its own relevant key.
 # example for COMPONENT_ID:
 # (__name__, DEFAULT_COMPONENT_KEY) -> default
 # (__name__, 4) -> custom
